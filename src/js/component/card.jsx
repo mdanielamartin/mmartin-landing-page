5a524c6360aee12286f7c30d7c60224c2766bb68
Maria Daniela Martin <m.danielamartin@proton.me>
2024-12-14 00:08:40 -0400

    used map for card render

--- src/js/component/card.jsx
+++ src/js/component/card.jsx
@@ -1,15 +1,14 @@
 import React from "react";
-import Rigo from "../../img/rigo-baby.jpg";
 
 
 
 //create your first component
-const Card = () => {
+const Card = ({ image, title }) => {
     return (
         <div className="card w-100 h-100">
-            < img className="card-img-top" src={Rigo} alt="Card image cap" />
+            < img className="card-img-top" src={image} alt="Card image cap" />
             <div className="card-body">
-                <h5 className="card-title mt-2">Card title</h5>
+                <h5 className="card-title mt-2">{title}</h5>
                 <p className="card-text">Lorem ipsum dolor sit amet, consectetur adipiscing elit. Nulla egestas pretium tortor, nec ultricies magna. Proin sodales augue vel venenatis consectetur. Cras tincidunt, dui a convallis tristique, ipsum felis tempor libero, quis tempus eros nisi in nibh. </p>
             </div>
             <div className='card-footer d-flex justify-content-center align-items-center pt-3 pb-3'>
--- src/js/component/home.jsx
+++ src/js/component/home.jsx
@@ -1,12 +1,29 @@
-import React from "react";
+import React, { useState } from "react";
 import NavBar from "./navbar.jsx";
 import Jumbotron from "./jumbotron.jsx"
 import Card from "./card.jsx";
 import Footer from "./footer.jsx";
+import img1 from "../../img/card1.jpg"
+import img2 from "../../img/card2.jpg"
+import img3 from "../../img/card3.jpg"
+import img4 from "../../img/card4.jpg"
 
 
 //create your first component
 const Home = () => {
+	const [cardList, setList] = useState([{
+		image: img1,
+		title: 'Card 1'
+	}, {
+		image: img2,
+		title: 'Card 2'
+	}, {
+		image: img3,
+		title: 'Card 3'
+	}, {
+		image: img4,
+		title: 'Card 4'
+	}])
 	return (
 		<div>
 			<NavBar />
@@ -14,22 +31,14 @@ const Home = () => {
 				<Jumbotron />
 				<div className='container-fluid d-flex justify-content-center mt-4 p-0 '>
 					<div className='row card-container w-100 g-4 '>
-						<div className='col-sm-12 col-lg-3 col-md-6'>
-							<Card />
-						</div>
-						<div className='col-sm-12 col-lg-3 col-md-6'>
-							<Card />
-						</div>
-						<div className='col-sm-12 col-lg-3 col-md-6 '>
-							<Card />
-						</div>
-						<div className='col-sm-12 col-lg-3 col-md-6 '>
-							<Card />
-						</div>
+						{cardList.map((item, index) => {
+							return (<div key={index} className='col-sm-12 col-lg-3 col-md-6'>
+								<Card image={item.image} title={item.title} /></div>)
+						})}
 					</div>
 				</div>
-				<Footer />
 			</div>
+			<Footer />
 		</div>
 	);
 };
